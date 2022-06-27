965abb6a394b67ea7080ccfb5dad18f75a4737ed
Hritik Kumar Sharma <hritix@gmail.com>
2022-06-28 01:07:56 +0530

    Facade  Deisgn Pattern

--- design-patterns/src/structural/facade/email/stationary/StationaryFactory.java
+++ design-patterns/src/structural/facade/email/stationary/StationaryFactory.java
@@ -3,6 +3,6 @@ package structural.facade.email.stationary;
 public class StationaryFactory {
 
 	public static Stationary createStationary() {
-		return new com.coffeepoweredcrew.facade.email.HalloweenStationary();
+		return new HalloweenStationary();
 	}
 }
